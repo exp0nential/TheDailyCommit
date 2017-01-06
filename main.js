510f5a8a5ed904eab05130cd6d92f67bf74cfeb8
John Doe <michaelcrowe@amdg.rockhursths.edu>
2017-01-06 11:28:16 -0600

    sign in/out

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+//redirects and fixes sign in bug
+if (window.location.hostname === '127.0.0.1') {
+    window.location = 'http://localhost:1898';
 }
-if (auth2.isSignedIn.get()) {
-  var profile = auth2.currentUser.get().getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
+
+var auth2;
+
+function appStart(){
+    gapi.load('auth2', initSigninV2);
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    $('.g-signin2').hide();
+    $('#signout').show();
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    $('.g-signin2').show();
+    $('#signout').hide();
 }
