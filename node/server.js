314218ac3df71d909f99035b3e8bc486d8dfe919
John Doe <michaelcrowe@amdg.rockhursths.edu>
2017-01-25 11:14:17 -0600

    Node Folder

---
+++ node/server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var Mongo = require('mongodb').MongoClient;
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongo_url = 'mongodb://localhost:27017/apcsp';
+
+    //static MongoDB operations
+
+Mongo.connect(mongo_url, function(err, db) {
+    if (err){
+        console.log('MongoDB connection error');
+    } else{
+        console.log ('Connected to MongoDB');
+    }
+});
+
+var app = express();
+
+//body parsing middleware (necessary to parse json recieved from clinets)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req,res) {
+    res.send('hello father');
+});
+
+//server on port 3000
+app.listen(3000);
+console.log('listening on 3000');
